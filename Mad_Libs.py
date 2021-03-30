f6f5608e39eae1d314fe850c4a92bd27380bbc7b
Vijay Saran <59793973+vijay0707@users.noreply.github.com>
2021-03-30 11:11:29 +0530

    Add files via upload

---
+++ Mad_Libs.py
@@ -0,0 +1,12 @@
+# Mad Libs Generator
+
+animals = input('enter a animal name : ')
+profession = input('enter a profession name: ')
+cloth = input('enter a piece of cloth name: ')
+things = input('enter a thing name: ')
+name= input('enter a name: ')
+place = input('enter a place name: ')
+verb = input('enter a verb in ing form: ')
+food = input('food name: ')
+print('say ' + food + ', the photographer said as the camera flashed! ' + name + ' and I had gone to ' + place +' to get our photos taken on my birthday. The first photo we really wanted was a picture of us dressed as ' + animals + ' pretending to be a ' + profession + '. when we saw the second photo, it was exactly what I wanted. We both looked like ' + things + ' wearing ' + cloth + ' and ' + verb + ' --exactly what I had in mind')
+
